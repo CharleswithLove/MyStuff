e81a5143654ed9965e8092dcf947c948121c9c85
Charles <CharlesLClark716@outlook.com>
2021-02-17 21:35:47 -0600

    intial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<style>
+	</style>
+</head>
+
+<body>
+	<h1 class="red" id="h1tag">The table element</h1>
+	<table id="practice_table">
+		<thead>
+			<tr>
+				<th>month</th>
+				<th>savings</th>
+			</tr>
+		</thead>
+		<tbody>
+
+			<tr>
+				<td>October</td>
+				<td>$200</td>
+			</tr>
+			<tr>
+				<td>November</td>
+				<td>$100</td>
+			</tr>
+			<tr>
+				<td>November</td>
+				<td>$100</td>
+			</tr>
+		<tr>
+			<td>November</td>
+			<td>$100</td>
+		</tr>
+		<tr>
+			<td>November</td>
+			<td>$100</td>
+		</tr>
+	</tbody>
+	</table>
+</body>
+
+</html>
